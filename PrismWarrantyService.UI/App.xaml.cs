617455f25781809acb6db4dcd1d58c560dcc2cc4
Vitaliy Sanikovich <vitaliysanikovich@gmail.com>
2017-11-01 01:07:51 +0300

    Authentification fixed

--- PrismWarrantyService.UI/App.xaml.cs
+++ PrismWarrantyService.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using PrismWarrantyService.UI.Services.Authentification.Concrete;
+using System;
+using System.Windows;
 
 namespace PrismWarrantyService.UI
 {
@@ -7,6 +9,18 @@ namespace PrismWarrantyService.UI
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            CreateCustomPrincipal();
+            RunBootstrapper();
+        }
+
+        private void CreateCustomPrincipal()
+        {
+            CustomPrincipal customPrincipal = new CustomPrincipal();
+            AppDomain.CurrentDomain.SetThreadPrincipal(customPrincipal);
+        }
+
+        private void RunBootstrapper()
+        {
             var bootstrapper = new Bootstrapper();
             bootstrapper.Run();
         }
