29fd94af502fbda5a69fbe5a09b1fdfc41a78789
Vitaliy Sanikovich <vitaliysanikovich@gmail.com>
2017-11-04 00:08:13 +0300

    Problems with ObservableCollection and IQueryable

--- PrismWarrantyService.UI/ViewModels/Clients/ClientsViewModel.cs
+++ PrismWarrantyService.UI/ViewModels/Clients/ClientsViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using Prism.Mvvm;
 using PrismWarrantyService.Domain.Abstract;
 using PrismWarrantyService.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace PrismWarrantyService.UI.ViewModels.Clients
 {
@@ -22,16 +21,23 @@ namespace PrismWarrantyService.UI.ViewModels.Clients
         {
             repository = repo;
             Clients = new ObservableCollection<Client>(repository.Clients);
+            Orders = new ObservableCollection<Order>();
         }
         #endregion
 
         #region Properties
         public ObservableCollection<Client> Clients { get; set; }
 
+        public ObservableCollection<Order> Orders { get; set; }
+
         public Client SelectedClient
         {
             get { return selectedClient; }
-            set { SetProperty(ref selectedClient, value); }
+            set
+            {
+                SetProperty(ref selectedClient, value);
+                RefreshClientOrders();
+            }
         }
         #endregion
 
@@ -39,6 +45,18 @@ namespace PrismWarrantyService.UI.ViewModels.Clients
         #endregion
 
         #region Methods
+        private void RefreshClientOrders()
+        {
+            // эта штука не хочет работать
+            //Orders = (ObservableCollection<Order>)repository
+            //    .Orders
+            //    .Where(x => x.Client == SelectedClient);
+
+            Orders.Clear();
+            Orders.Add(new Order() { Summary = "Этот список имитирует" });
+            Orders.Add(new Order() { Summary = "список заказов клиента" });
+            Orders.Add(new Order() { Summary = SelectedClient.Department + " " + SelectedClient.Company });
+        }
         #endregion
     }
 }
